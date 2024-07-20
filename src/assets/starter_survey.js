c1fcac4eb174bb692dc7bb9df2e914307489fe68
Luca Staffner <staffnerluca@gmail.com>
2024-07-20 10:59:41 +0200

    started starter quiz

---
+++ src/assets/starter_survey.js
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function StarterQuiz() {
+  const [userAtributes, setUserAttributes] = useState(0)
+  function aboutYouForm(){
+
+  }
+
+  
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default App
