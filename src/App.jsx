a2db9519b09d24cc04ed6249e42b609d330218b4
Luca Staffner <staffnerluca@gmail.com>
2024-08-17 07:17:20 +0200

    first draft of testAPI

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import VegetarianismStreak from './components/VegitarianismStreak';
 import CoursesOverview from './components/CoursesOverview';
 import DidYouEatMeatToday from './components/DidYouEatMeatToday';
+import TestAPI from './components/TestAPI';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route exact path="/vegitarianismStreak" Component={VegetarianismStreak} />
           <Route path="/start" Component={CoursesOverview} />
           <Route path="/didYouEatMeat" Component={DidYouEatMeatToday} />
+          <Route path="/testAPI" Component={TestAPI} />
         </Routes>
       </div>
     </Router>
