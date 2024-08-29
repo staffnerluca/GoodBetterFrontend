2a829d3375db38ac96f8ed7b0caf547095e44f80
Luca Staffner <staffnerluca@gmail.com>
2024-08-29 09:25:20 +0200

    api request in Header

--- src/components/VegStreakContext.jsx
+++ src/components/VegStreakContext.jsx
@@ -1,10 +1,27 @@
-// VegStreakContext.jsx
-import React, { createContext, useState, useContext } from "react";
+import React, { createContext, useState, useContext, useEffect } from "react";
 
 const VegStreakContext = createContext();
 
 export function VegStreakProvider({ children }) {
   const [currentVegStreak, setCurrentVegStreak] = useState(0);
+  useEffect(() => {
+    const fetchVegStreak = async () => {
+      try {
+        const response = await fetch("http://127.0.0.1:8000/api/get_current_streak/username1");
+        if (response.ok) {
+          const data = await response.json();
+          setCurrentVegStreak(data.not_eating_meat_streak);
+        } else {
+          console.error("Failed to fetch streak");
+        }
+      } catch (error) {
+        console.error("Error fetching streak:", error);
+      }
+    };
+
+    fetchVegStreak();
+  }, []);
+
 
   const value = {
     currentVegStreak,
